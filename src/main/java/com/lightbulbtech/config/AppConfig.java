a4fd61f6774fdc21f0c2c208a1ad2b847512a822
shiddiqeuy <shiddiqitt@gmail.com>
2017-06-17 10:07:34 +0700

    first commit

---
+++ src/main/java/com/lightbulbtech/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.lightbulbtech.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.lightbulbtech.controller"})
+public class AppConfig {
+}
